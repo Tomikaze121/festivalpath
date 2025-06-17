0d86e5e2fa0d12db9b0f30dbb271a8f08c3d8141
JC <Jeanclaude.lequigan.22@usjr.edu.ph>
2025-06-18 01:35:50 +0800

    fixes

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -5,29 +5,53 @@ import mapboxgl from 'mapbox-gl';
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
-  standalone:false,
+  standalone: false,
 })
 export class MapPage implements AfterViewInit {
   map!: mapboxgl.Map;
 
   ngAfterViewInit(): void {
-    
     mapboxgl.accessToken = 'pk.eyJ1IjoidG9taWthemUxIiwiYSI6ImNtYzAwdG81djFjdGsycXNiaHB6N3gxZHgifQ.BRrWeRX1vv7PdksH9RN42w';
 
-    
-    setTimeout(() => {
-      this.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [121.0, 14.5],
-        zoom: 15,
-      });
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/tomikaze1/cmc0s61t0000601sr48iu3n18',
+      center: [121.0, 14.5],
+      zoom: 15,
+    });
 
-      this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(new mapboxgl.NavigationControl());
+    this.map.addControl(new mapboxgl.FullscreenControl());
+    this.map.addControl(new mapboxgl.ScaleControl());
 
-      this.map.on('load', () => {
-        new mapboxgl.Marker().setLngLat([121.0, 14.5]).addTo(this.map);
-      });
-    }, 100); 
+    this.map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+
+    this.map.on('load', () => {
+      this.map.resize(); 
+
+      new mapboxgl.Marker({ color: 'orange' })
+        .setLngLat([121.0005, 14.5005])
+        .setPopup(new mapboxgl.Popup({ offset: 25 }).setText('Main Stage 🎤'))
+        .addTo(this.map);
+
+      new mapboxgl.Marker({ color: 'purple' })
+        .setLngLat([121.002, 14.502])
+        .setPopup(new mapboxgl.Popup({ offset: 25 }).setText('Food Court 🍔'))
+        .addTo(this.map);
+    });
+  }
+
+  ionViewDidEnter() {
+    if (this.map) {
+      this.map.resize(); 
+    }
   }
 }
